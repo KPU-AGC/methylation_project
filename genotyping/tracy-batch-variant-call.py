2a1d70534973d86264cd84f6231e5f44431142d8
Michael Ke <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-06-02 12:27:25 -0700

    Fixed the header again my god

--- genotyping/tracy-batch-variant-call.py
+++ genotyping/tracy-batch-variant-call.py
@@ -194,7 +194,7 @@ def output_summary(output_path, best_runs, failed_runs, sample_info):
     csv_file = open(csv_output_path, 'w')
     csv_writer = csv.writer(csv_file, delimiter=',')
     csv_writer.writerow(('Best runs',))
-    csv_writer.writerow(('Sample', 'Trace score', 'Median PUP, Left Trim, Right Trim'))
+    csv_writer.writerow(('Sample', 'Trace score', 'Median PUP', 'Left Trim', 'Right Trim'))
     csv_writer.writerows(best_runs)
     csv_writer.writerow(('Failed runs',))
     csv_writer.writerow(('Sample', 'Trace score', 'Median PUP'))

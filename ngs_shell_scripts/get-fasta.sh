5759d74daa86b218ab375340f3df1cd007bc8a1f
Michael Ke <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-07-12 13:53:58 -0700

    Create get-fasta.sh
    Script to get bed and fasta output

---
+++ ngs_shell_scripts/get-fasta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Set correct directories
+#CHANGE genome and output_name TO FIT WHAT YOU NEED
+data=../data
+results=../results
+fasta=$results/fasta
+input_name=$data/bspcr-primers.tab
+genome=$data/Bos_taurus.ARS-UCD1.2.dna.toplevel.fa
+bed_output=$results/BSPCR-primers.bed
+fasta_output=$results/BSPCR-all-primers.fasta
+
+[ ! -d $fasta ] && mkdir -p $fasta
+
+bedtools getfasta -fi $genome -bed $input_name -bedOut > $bed_output
+bedtools getfasta -fi $genome -bed $input_name -name > $fasta_output
+
+python ./split-fasta.py $fasta_output --output $fasta

17babbbeac09ee65701a2095a14bf4d2f5c87115
Michael Ke <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-07-12 13:54:35 -0700

    Update ngs-process-bismark.sh
    Turned process into a pipeline

--- ngs_shell_scripts/ngs-process-bismark.sh
+++ ngs_shell_scripts/ngs-process-bismark.sh
@@ -2,10 +2,22 @@
 #Set correct directories
 data=../data
 results=../results
-coverage=$data/coverage
 bspcr_bed=$data/BSPCR_regions.bed
+sample_summary=$results/sample_summary
+primer_summary=$results/primer_summary
+detailed=$results/detailed
 
-for file in $coverage/*.cov; do 
+[ ! -d $sample_summary ] && mkdir -p $sample_summary
+[ ! -d $primer_summary ] && mkdir -p $primer_summary
+[ ! -d $detailed ] && mkdir -p $detailed
+
+for file in $data/*.cov; do 
     echo $file
-    python ./process-bismark-coverage.py $file $bspcr_bed -o $results
+    python ./process-bismark-coverage.py $file $bspcr_bed -o $detailed
 done
+
+mv $detailed/*_summary.csv $sample_summary
+
+python ./assess-by-primer.py $sample_summary --output $primer_summary
+
+
